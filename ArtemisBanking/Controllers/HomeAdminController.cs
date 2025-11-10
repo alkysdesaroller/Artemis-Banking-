6c1e70c414239aeb4d55db4da5a7386b68fe17fe
Luis Angel Ureña Perdomo <urenaperdomo@gmail.com>
2025-11-10 00:54:00 -0400

    Se agrego el Authorize con el rol adecuado en los homes

--- ArtemisBanking/Controllers/HomeAdminController.cs
+++ ArtemisBanking/Controllers/HomeAdminController.cs
@@ -1,7 +1,10 @@
+using ArtemisBanking.Core.Domain.Common.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ArtemisBanking.Controllers;
 
+[Authorize(Roles = $"{nameof(Roles.Admin)}")] // Recuerda leer el apartado de seguridad de los requerimientos
 public class HomeAdminController : Controller
 {
     // GET
--- ArtemisBanking/Controllers/HomeAtmController.cs
+++ ArtemisBanking/Controllers/HomeAtmController.cs
@@ -1,7 +1,10 @@
+using ArtemisBanking.Core.Domain.Common.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ArtemisBanking.Controllers;
 
+[Authorize(Roles = $"{nameof(Roles.Atm)}")] // Recuerda leer el apartado de seguridad de los requerimientos
 public class HomeAtmController : Controller
 {
     // GET
