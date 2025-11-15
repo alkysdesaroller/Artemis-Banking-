f70111bab39e33d4a426cae097521ba2a141839e
AlnaDesarroller <natansanchez732@gmail.com>
2025-11-14 23:36:35 -0400

    Revert

--- ArtemisBanking.Core.Application/Interfaces/ILoanService.cs
+++ ArtemisBanking.Core.Application/Interfaces/ILoanService.cs
@@ -7,12 +7,12 @@ public interface ILoanService : IGenericService<string, LoanDto>
     // Task<Result<List<LoanDto>>> GetByUserIdAsync(string userId, bool? isActive = null);
     // Task<Result> UpdateInterestRateAsync(int id, decimal newRate);
     // Task<Result<LoanDto>> AssignLoanAsync(AssignLoanDto dto);
-    // Task<Result<decimal>> GetAverageClientDebtAsync();
     // Task<Result<bool>> IsHighRiskClientAsync(string userId, decimal newLoanAmount);
     // Task<Result<bool>> HasActiveLoanAsync(string userId);
     // Task<Result<LoanDto>> GetByLoanNumberAsync(string loanNumber);
     // Task<Result<string>> GenerateUniqueLoanNumberAsync();
     
+    Task<Result<decimal>> GetAverageClientDebtAsync();
     
      Task<PaginatedData<LoanSummaryDto>> GetLoansPagedAsync(int page, int pageSize, string? identityCardNumber = null, bool? isCompleted = null);
      
